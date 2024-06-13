432bcc7df51de6cb9984b93e3287c591bae17d1a
Sonali Goel <112942761+goelsonali@users.noreply.github.com>
2024-06-13 22:35:27 +0100

    swagger-ui configured
    swagger-ui configured with backend

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.2.5"
     id("io.spring.dependency-management") version "1.1.4"
+    id("org.springdoc.openapi-gradle-plugin") version "1.8.0"
 }
 
 group = "com.wcc.cms"
@@ -28,7 +29,7 @@ dependencies {
 
 
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("io.springfox:springfox-boot-starter:3.0.0")
+    implementation ("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
