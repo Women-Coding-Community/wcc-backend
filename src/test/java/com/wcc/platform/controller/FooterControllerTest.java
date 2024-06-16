b387d2a708be4e6a7f51ebe03c698d970e9bf9b1
Sonali Goel <112942761+goelsonali@users.noreply.github.com>
2024-06-16 23:32:50 +0100

    Positive test for webMvc

--- src/test/java/com/wcc/platform/controller/FooterControllerTest.java
+++ src/test/java/com/wcc/platform/controller/FooterControllerTest.java
@@ -1,5 +1,8 @@
 package com.wcc.platform.controller;
 
+import com.wcc.platform.domain.cms.attributes.LabelLink;
+import com.wcc.platform.domain.cms.attributes.Network;
+import com.wcc.platform.domain.cms.pages.FooterPage;
 import com.wcc.platform.domain.exceptions.PlatformInternalException;
 import com.wcc.platform.service.CmsService;
 import org.junit.jupiter.api.Test;
@@ -7,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
 
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
@@ -34,7 +39,24 @@ public class FooterControllerTest {
                 .andExpect(jsonPath("$.status", is(500)))
                 .andExpect(jsonPath("$.message", is("Invalid Json")))
                 .andExpect(jsonPath("$.details", is("uri=/api/cms/v1/footer")));
+    }
 
+    @Test
+    void testOKResponse() throws Exception {
+        when(mockCmsService.getFooter()).thenReturn(new FooterPage("footer_title","footer_subtitle",
+                "footer_desc", List.of(new Network("net_type","net_link" )),
+                new LabelLink("label_title", "label", "label_uri")));
 
+        mockMvc.perform(get("/api/cms/v1/footer")
+                        .contentType(APPLICATION_JSON))
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$.title", is("footer_title")))
+                        .andExpect(jsonPath("$.subtitle", is("footer_subtitle")))
+                        .andExpect(jsonPath("$.description", is("footer_desc")))
+                        .andExpect(jsonPath("$.network[0].type", is("net_type")))
+                        .andExpect(jsonPath("$.network[0].link", is("net_link")))
+                        .andExpect(jsonPath("$.link.title", is("label_title")))
+                        .andExpect(jsonPath("$.link.label", is("label")))
+                        .andExpect(jsonPath("$.link.uri", is("label_uri")));
     }
 }
