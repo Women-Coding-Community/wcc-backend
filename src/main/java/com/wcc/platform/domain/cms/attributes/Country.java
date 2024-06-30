be305165e1270225f675c6205be29e0b5def38e5
Nevena Verbic <nevena.verbic@gmail.com>
2024-06-30 12:26:34 +0200

    Remove TODO

--- src/main/java/com/wcc/platform/domain/cms/attributes/Country.java
+++ src/main/java/com/wcc/platform/domain/cms/attributes/Country.java
@@ -1,5 +1,4 @@
 package com.wcc.platform.domain.cms.attributes;
 
-// TODO: set values to default to empty strings in case not both params provided
 public record Country(String countryCode, String countryName) {
 }
