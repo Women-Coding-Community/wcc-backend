a6460f75fdf9571f1946a53226a9625e7902e9b1
Adriana Zencke <adriana.zencke@centricsoftware.com>
2024-07-02 13:06:54 +0200

    Fix conflicts

--- src/main/java/com/wcc/platform/domain/platform/LeadershipMember.java
+++ src/main/java/com/wcc/platform/domain/platform/LeadershipMember.java
@@ -7,18 +7,12 @@ import lombok.NoArgsConstructor;
 
 /**
  * Represents the core team of the community: {@link MemberType#DIRECTOR}, {@link MemberType#LEADER}
-<<<<<<< HEAD
  * and {@link MemberType#EVANGELIST}.
-=======
- * and {@link MemberType#EVANGELIST}
->>>>>>> 99dacc76e5849b960f2eae2a18c8c44e2edc091e
  */
 @Data
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class LeadershipMember extends Member {
-  
-  @JsonIgnore
-  private MemberType memberType;
 
+  @JsonIgnore private MemberType memberType;
 }
