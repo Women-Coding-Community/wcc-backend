98d57bedb48a4bc720a3b6148742b4aaa250654f
Nevena Verbic <nevena.verbic@gmail.com>
2024-08-11 20:03:20 +0200

    Commit solves the problem of writing to a file. It is not possible to write to files in the resources repository. Add file:storage:directory: /app/data path in application properties file for use when run in the docker container. Add environment variable to check if the app is running in docker container (temporary solution - better to create two configurations for development and production) Save members to file /data/members.json. If running inside of docker container file path is ./app/data/members.json

--- src/main/java/com/wcc/platform/configuration/RepositoryConfig.java
+++ src/main/java/com/wcc/platform/configuration/RepositoryConfig.java
@@ -3,6 +3,7 @@ package com.wcc.platform.configuration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wcc.platform.repository.MemberRepository;
 import com.wcc.platform.repository.file.FileMemberRepository;
+import java.io.IOException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,10 @@ public class RepositoryConfig {
   /** Create FileMemberRepository bean. */
   @Bean
   public MemberRepository createFileRepository(final ObjectMapper objectMapper) {
-    return new FileMemberRepository(objectMapper);
+    try {
+      return new FileMemberRepository(objectMapper);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
