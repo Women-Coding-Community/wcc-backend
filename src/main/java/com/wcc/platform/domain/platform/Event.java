efa1660a489eedd456bfc1a9badb8fa670d557d7
Hasan Diwan <hasan@gooutside.com>
2024-11-12 13:26:24 -0800

    allow for separator using dots or colons

--- src/main/java/com/wcc/platform/domain/platform/Event.java
+++ src/main/java/com/wcc/platform/domain/platform/Event.java
@@ -1,9 +1,11 @@
 package com.wcc.platform.domain.platform;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.wcc.platform.domain.cms.attributes.EventResource;
 import com.wcc.platform.domain.cms.attributes.EventType;
 import com.wcc.platform.domain.cms.attributes.Image;
 import com.wcc.platform.domain.cms.attributes.LabelLink;
+import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
@@ -26,12 +28,22 @@ public class Event {
   private String title;
   private String description;
   private EventType eventType;
-  private String startDate; // TODO convert to Date
-  private String endDate; // TODO convert to Date
+  //private String startDate; // TODO convert to Date
+  private Instant startDate;
+  //private String endDate; // TODO convert to Date
+  private Instant endDate;
   private ProgramType topics;
   private List<Image> images;
   private LabelLink speakerProfile;
   private LabelLink hostProfile;
   private LabelLink eventLink;
   private List<EventResource> eventResources;
+  public setEndDate(String dateFormat) {
+    this.endDate = DateTimeFormatter.ISO_INSTANT.parse(dateFormat.replace(".", ":"));
+  }
+
+  public setStartDate(String dateFormat) {
+    this.startDate = DateTimeFormatter.ISO_INSTANT.parse(dateFormat.replace(".", ":"));
+  }
+
 }
