d8e23a39af5280aac11479bb55a1640757d2827d
Eleonora Belova <eleonora.belova@Eleonoras-MacBook-Air.local>
2024-11-18 15:39:10 +0100

    feat: improved playwright config, fixed scripts and improved test cases implementation

--- integration-test/tests/cms/footer.test.ts
+++ integration-test/tests/cms/footer.test.ts
@@ -1,48 +1,14 @@
-import {expect, request, test} from '@playwright/test';
+import {expect, test} from '@playwright/test';
+import { footerExpectedInformation } from '../../utils/datafactory/footer.data';
 
-test('GET /api/cms/v1/footer returns correct footer data', async () => {
-  const context = await request.newContext();
-  const response = await context.get(`/api/cms/v1/footer`);
+test('GET /api/cms/v1/footer returns correct footer data', async ({request}) => {
+  const response = await request.get(`/api/cms/v1/footer`);
 
   expect(response.status()).toBe(200);
 
   const body = await response.json();
 
-  const expectedResponse = {
-    "title": "Follow Us",
-    "subtitle": "Join us on social media and stay tuned.",
-    "description": "Women Coding Community is a not-for-profit organisation. © 2024 Women Coding Community",
-    "network": [{
-        "type": "linkedIn",
-        "link": "https://www.linkedin.com/company/womencodingcommunity"
-      },
-      {
-        "type": "twitter",
-        "link": "https://twitter.com/WCC_Community"
-      },
-      {
-        "type": "github",
-        "link": "https://github.com/WomenCodingCommunity"
-      },
-      {
-        "type": "instagram",
-        "link": "https://www.instagram.com/women_coding_community"
-      },
-      {
-        "type": "email",
-        "link": "london@womencodingcommunity.com"
-      },
-      {
-        "type": "slack",
-        "link": "https://join.slack.com/t/womencodingcommunity/shared_invite/zt-2hpjwpx7l-rgceYBIWp6pCiwc0hVsX8A"
-      }
-    ],
-    "link": {
-      "title": "Experiencing Technical Issues?",
-      "label": "Send us a report",
-      "uri": "https://github.com/WomenCodingCommunity/WomenCodingCommunity.github.io/issues/new?template=bug_report.md&title=bug%20title"
-    }
-  }
+  const expectedResponse = footerExpectedInformation;
 
   expect(body).toEqual(expectedResponse);
 });
--- integration-test/tests/openapi/openapi.test.ts
+++ integration-test/tests/openapi/openapi.test.ts
@@ -1,9 +1,8 @@
-import {expect, request, test} from '@playwright/test';
+import {expect, test} from '@playwright/test';
 
-test('GET /swagger-ui/index.html', async () => {
-  const context = await request.newContext();
+test('GET /swagger-ui/index.html', async ({request}) => {
 
-  const response = await context.get(`/swagger-ui/index.html`);
+  const response = await request.get(`/swagger-ui/index.html`);
 
   expect(response.status()).toBe(200);
 });
