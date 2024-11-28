6cc136aba5ccfde47a3c8da3e19ca41905b36ff3
nimat1 <purnima.tyagi@gmail.com>
2024-11-28 17:57:58 +0000

    correct url for events

--- integration-test/tests/cms/events.test.ts
+++ integration-test/tests/cms/events.test.ts
@@ -1,7 +1,7 @@
 import {expect, test} from '@playwright/test';
 import { eventsExepctedInformation } from '../../utils/datafactory/events.data';
-test('GET /api/cms/v1/code-of-conduct returns correct team data', async ({request}) => {
-    const response = await request.get(`/api/cms/v1/footer`);
+test('GET /api/cms/v1/events returns correct data', async ({request}) => {
+    const response = await request.get(`/api/cms/v1/events`);
  
     expect(response.status()).toBe(200);
    
