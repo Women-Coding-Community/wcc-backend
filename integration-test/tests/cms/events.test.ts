89bc47ce9ad50fd4895077d53000c510e6635c53
nimat1 <purnima.tyagi@gmail.com>
2024-12-03 14:11:32 +0000

    Events API integration test success case

--- integration-test/tests/cms/events.test.ts
+++ integration-test/tests/cms/events.test.ts
@@ -3,7 +3,4 @@ test('GET /api/cms/v1/events returns correct data', async ({request}) => {
     const response = await request.get(`/api/cms/v1/events`);
     // response status validation
     expect(response.status()).toBe(200);
-    const body = await response.json();
-  });
-  
-   
+     });
