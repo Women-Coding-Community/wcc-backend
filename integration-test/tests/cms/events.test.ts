a64e84d12a862862842502c4842e896323a4b04c
Adriana Zenck Zimmermann <dricazenck@gmail.com>
2024-12-03 15:50:31 +0100

    Apply suggestions from code review

--- integration-test/tests/cms/events.test.ts
+++ integration-test/tests/cms/events.test.ts
@@ -1,5 +1,5 @@
 import {expect, test} from '@playwright/test';
-test('GET /api/cms/v1/events returns correct data', async ({request}) => {
+test('GET /api/cms/v1/events returns success response code', async ({request}) => {
     const response = await request.get(`/api/cms/v1/events`);
     expect(response.status()).toBe(200);
      });
