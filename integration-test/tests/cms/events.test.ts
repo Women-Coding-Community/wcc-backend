d9aa5175e67271c5611fbb8b3be74308d711964c
Adriana Zenck Zimmermann <dricazenck@gmail.com>
2024-12-03 15:51:33 +0100

    Adjust code format

--- integration-test/tests/cms/events.test.ts
+++ integration-test/tests/cms/events.test.ts
@@ -1,5 +1,6 @@
 import {expect, test} from '@playwright/test';
+
 test('GET /api/cms/v1/events returns success response code', async ({request}) => {
     const response = await request.get(`/api/cms/v1/events`);
     expect(response.status()).toBe(200);
-     });
+});
