3d7e583a7308cce928cb76299c83fc966d2e320c
nimat1 <purnima.tyagi@gmail.com>
2024-12-08 10:25:57 +0000

    Integration test collabrorators

--- integration-test/tests/cms/collaborators.test.ts
+++ integration-test/tests/cms/collaborators.test.ts
@@ -1,4 +1,5 @@
 import {expect, test} from '@playwright/test';
+
 test('GET /api/cms/v1/collaborators returns correct data', async ({request}) => {
 const response = await request.get('/api/cms/v1/collaborators');
 expect(response.status()).toBe(200);
