2bc1269b5a532346f7704005f466a136e218f9ba
Adriana Zenck Zimmermann <dricazenck@gmail.com>
2024-12-19 12:26:21 +0100

    revert change

--- integration-test/playwright.config.ts
+++ integration-test/playwright.config.ts
@@ -9,6 +9,12 @@ dotenv.config();
 export default defineConfig({
   projects: [
     {
+      name: 'api_dev',
+      use: { 
+        baseURL: process.env.API_HOST
+       },
+    },
+    {
       name: 'api_local',
       use: {
         baseURL: 'http://localhost:8080'
