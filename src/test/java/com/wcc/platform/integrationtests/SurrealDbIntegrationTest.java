dd3595b3e1c155ded7a3d475cb8b2c111a02a32a
muler-hussel <hazel_faye@outlook.com>
2024-12-22 05:09:37 +0800

    change to one driver

--- src/test/java/com/wcc/platform/integrationtests/SurrealDbIntegrationTest.java
+++ src/test/java/com/wcc/platform/integrationtests/SurrealDbIntegrationTest.java
@@ -4,8 +4,6 @@ import static com.wcc.platform.factories.SetupFactories.createLinkTest;
 import static com.wcc.platform.factories.SetupFactories.createNetworksTest;
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.surrealdb.connection.SurrealConnection;
-import com.surrealdb.connection.SurrealWebSocketConnection;
 import com.surrealdb.driver.SyncSurrealDriver;
 import com.wcc.platform.domain.cms.PageType;
 import com.wcc.platform.domain.cms.attributes.LabelLink;
@@ -15,12 +13,15 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
+@SpringBootTest
 @Testcontainers
 class SurrealDbIntegrationTest {
 
@@ -29,6 +30,7 @@ class SurrealDbIntegrationTest {
           .withExposedPorts(8000)
           .withCommand("start --user root --pass root");
   private static final String TABLE = "page";
+  @Autowired private SyncSurrealDriver driver;
 
   @DynamicPropertySource
   static void registerSurrealDbProperties(final DynamicPropertyRegistry registry) {
@@ -43,27 +45,8 @@ class SurrealDbIntegrationTest {
     registry.add("surrealdb.database", () -> "test_db");
   }
 
-  @BeforeAll
-  static void setUp() {
-    SURREAL_DB_CONTAINER.start();
-  }
-
-  @AfterAll
-  static void tearDown() {
-    SURREAL_DB_CONTAINER.stop();
-  }
-
   @Test
   void testSaveAndFindAll() {
-    String host = SURREAL_DB_CONTAINER.getHost();
-    Integer port = SURREAL_DB_CONTAINER.getFirstMappedPort();
-
-    SurrealConnection connection = new SurrealWebSocketConnection(host, port, false);
-    connection.connect(120); // timeout second
-
-    SyncSurrealDriver driver = new SyncSurrealDriver(connection);
-    driver.signIn("root", "root");
-    driver.use("test", "test");
     SurrealDbPageRepository repository = new SurrealDbPageRepository(driver);
     // Arrange
     List<Network> networks = createNetworksTest();
@@ -92,7 +75,7 @@ class SurrealDbIntegrationTest {
     assertTrue(page.isPresent());
     assertEquals(6, page.get().size());
     assertTrue(page.get().containsValue(PageType.FOOTER.getPageId()));
+
     driver.delete(TABLE);
-    connection.disconnect();
   }
 }
