9132e589cc6c55a956f36e8f6de5404f17e80523
Eleonora Belova <eleonora.belova@Eleonoras-MacBook-Air.local>
2024-12-21 22:33:04 +0100

    fix: removed unnecessary comments

--- integration-test/eslint.config.js
+++ integration-test/eslint.config.js
@@ -3,23 +3,16 @@ const { configs: stylisticConfigs } = require('@typescript-eslint/eslint-plugin'
 
 module.exports = [
   {
-    // Specify the files to lint
     files: ['**/*.ts', '**/*.tsx'],
-
-    // Set the parser under languageOptions
     languageOptions: {
-      parser: require('@typescript-eslint/parser'), // Use the parser as an object
+      parser: require('@typescript-eslint/parser'), 
       parserOptions: {
-        project: './tsconfig.json', // Path to your tsconfig file
+        project: './tsconfig.json',
       },
     },
-
-    // Include plugins and rules directly
     plugins: {
       '@typescript-eslint': recommendedConfigs,
     },
-
-    // Combine rules from different configurations
     rules: {
       ...recommendedConfigs.rules,
       ...stylisticConfigs.rules,
