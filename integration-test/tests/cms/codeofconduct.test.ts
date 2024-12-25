5094285173a2eafde9c9a70d9e0a34e79c529f03
Eleonora Belova <eleonora.belova@Eleonoras-MacBook-Air.local>
2024-12-25 20:14:58 +0100

    feat: trying to commit a file which will fail

--- integration-test/tests/cms/codeofconduct.test.ts
+++ integration-test/tests/cms/codeofconduct.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test';
 import { codeofconductExepctedInformation } from '@utils/datafactory/codeofconduct.data';
 
-test('GET /api/cms/v1/code-of-conduct returns correct data', async ({ request }) => {
+test("GET /api/cms/v1/code-of-conduct returns correct data", async ({ request }) => {
   const response = await request.get(`/api/cms/v1/code-of-conduct`);
 
   expect(response.status()).toBe(200);
