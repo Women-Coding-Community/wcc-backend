129451f26aa776b0beaa8a771d829032b69fd68a
Eleonora Belova <65826199+nora-weisser@users.noreply.github.com>
2024-12-26 16:07:44 +0100

    Maintained Footer API Test Case and Updated schema validation (#185)
    * feat: updated required fields in footer and link schemas
    * feat: updated footer schema and improved schema validation
    
    ---------
    
    Co-authored-by: Eleonora Belova <eleonora.belova@Eleonoras-MacBook-Air.local>

--- integration-test/utils/datafactory/schemas/footer.schema.ts
+++ integration-test/utils/datafactory/schemas/footer.schema.ts
@@ -6,6 +6,10 @@ export const footerSchema = {
         footerSchema: {
             "type": "object",
             "properties": {
+                "id": {
+                    "type": "string",
+                    "const": "page:FOOTER"
+                },
                 "title": {
                     "type": "string",
                     "minLength": 1
@@ -32,7 +36,6 @@ export const footerSchema = {
                                 "minLength": 1
                             }
                         },
-                        "required": ["type", "link"],
                         "additionalProperties": false,
                         "if": {
                             "properties": {
@@ -62,6 +65,7 @@ export const footerSchema = {
                 "link": {...linkSchema.definitions.linkSchema}
             },
             "required": [
+                "id",
                 "title",
                 "subtitle",
                 "description",
