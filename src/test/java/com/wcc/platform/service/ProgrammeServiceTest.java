a3375e45b44d775cb59f62eb87f46f85b260e43c
Adriana Zencke Zimmermann <dricazenck@gmail.com>
2024-12-31 18:17:42 +0100

    feat: Update test for program page

--- src/test/java/com/wcc/platform/service/ProgrammeServiceTest.java
+++ src/test/java/com/wcc/platform/service/ProgrammeServiceTest.java
@@ -3,29 +3,31 @@ package com.wcc.platform.service;
 import static com.wcc.platform.domain.platform.ProgramType.BOOK_CLUB;
 import static com.wcc.platform.factories.SetupProgrammeFactories.createProgrammePageTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anyMap;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.wcc.platform.domain.cms.pages.programme.ProgrammePage;
 import com.wcc.platform.repository.PageRepository;
 import java.util.Map;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 /** Test class for ProgrammeService. */
 class ProgrammeServiceTest {
-  private ObjectMapper objectMapper;
   private ProgrammeService service;
+
   @Mock private PageRepository pageRepository;
+  @Mock private ObjectMapper objectMapper;
 
   @BeforeEach
   void setUp() {
-    objectMapper = Mockito.mock(ObjectMapper.class);
+    MockitoAnnotations.openMocks(this);
+
+    objectMapper = new ObjectMapper();
+    objectMapper.findAndRegisterModules();
+
     service = new ProgrammeService(pageRepository, objectMapper);
   }
 
@@ -33,10 +35,9 @@ class ProgrammeServiceTest {
   @SuppressWarnings("unchecked")
   void whenGetTeamGivenRecordExistOnDatabaseThenReturnValidResponse() {
     var programmePage = createProgrammePageTest("bookClubPage.json");
-    var mapPage = new ObjectMapper().convertValue(programmePage, Map.class);
+    var mapPage = objectMapper.convertValue(programmePage, Map.class);
 
     when(pageRepository.findById(BOOK_CLUB.toPageId())).thenReturn(Optional.of(mapPage));
-    when(objectMapper.convertValue(anyMap(), eq(ProgrammePage.class))).thenReturn(programmePage);
 
     var response = service.getProgramme(BOOK_CLUB);
 
