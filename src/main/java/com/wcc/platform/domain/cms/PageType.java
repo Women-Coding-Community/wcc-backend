11ea53eff5bde378faf1571a675888cbe0c8b6b9
Liliia Rafikova <lilrafil@amazon.co.uk>
2025-01-17 15:38:34 +0000

    Initial commit for CelebrateHer page

--- src/main/java/com/wcc/platform/domain/cms/PageType.java
+++ src/main/java/com/wcc/platform/domain/cms/PageType.java
@@ -14,12 +14,11 @@ public enum PageType {
   PROG_BOOK_CLUB("bookClubPage.json", "page:BOOK_CLUB"),
   EVENT_FILTERS("eventsFiltersSection.json", "UNDEFINED"),
   ABOUT_US("aboutUsPage.json", "page:ABOUT_US"),
-  CELEBRATE_HER("celebrateHerPage.json", "page:CELEBRATE_HER_PAGE"),
   TEAM("teamPage.json", "page:TEAM"),
   COLLABORATOR("collaboratorPage.json", "page:COLLABORATORS"),
-  CODE_OF_CONDUCT("codeOfConductPage.json", "page:CODE_OF_CONDUCT");
+  CODE_OF_CONDUCT("codeOfConductPage.json", "page:CODE_OF_CONDUCT"),
+  CELEBRATE_HER("celebrateHerPage.json", "page:CELEBRATE_HER_PAGE");
 
-  public static final String ID_PREFIX = "page:";
 
   private final String fileName;
   private final String id;
--- src/main/java/com/wcc/platform/domain/cms/pages/aboutUs/CelebrateHerPage.java
+++ src/main/java/com/wcc/platform/domain/cms/pages/aboutUs/CelebrateHerPage.java
@@ -1,12 +1,25 @@
 package com.wcc.platform.domain.cms.pages.aboutUs;
 
+<<<<<<< HEAD
 import com.wcc.platform.domain.cms.pages.Page;
 import com.wcc.platform.domain.platform.Member;
+=======
+import com.wcc.platform.domain.cms.attributes.Contact;
+import com.wcc.platform.domain.cms.attributes.MemberByType;
+import com.wcc.platform.domain.cms.pages.Page;
+import com.wcc.platform.domain.cms.pages.Section;
+import com.wcc.platform.domain.platform.Member;
+import jakarta.validation.constraints.NotEmpty;
+>>>>>>> 72da30f (Initial commit for CelebrateHer page)
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
 /**
+<<<<<<< HEAD
  * CMS Celebrate Her page.
+=======
+ * CMS About Us page.
+>>>>>>> 72da30f (Initial commit for CelebrateHer page)
  *
  * @param page Page details as title and images
  * @param members all details
