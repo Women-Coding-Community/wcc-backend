5f805346c89aa48a6226ac725d266514d267fcd6
Eleonora Belova <eleonora.belova@Eleonoras-MacBook-Air.local>
2025-02-13 21:06:34 +0100

    feat: added negative scenarios for invalid headers

--- integration-test/tests/cms/mentorshipoverview.test.ts
+++ integration-test/tests/cms/mentorshipoverview.test.ts
@@ -21,3 +21,20 @@ test('GET /api/cms/v1/mentorship/overview returns correct data', async ({ reques
     }
   }
 });
+
+test.describe('unauthorized request with invalid headers', () => {
+  
+  const testData = [
+    { description: 'header is empty', headers: { 'X-API-KEY': '' }},
+    { description: 'header is invalid', headers: { 'X-API-KEY': 'invalid_key' }},
+  ];
+
+  testData.forEach(({ description, headers }) => {
+    test(`${description}`, async ({ request }) => {
+      const response = await request.get(`/api/cms/v1/mentorship/overview`, {
+        headers: headers
+      });
+      expect(response.status()).toBe(401);
+    });
+  });
+});
--- integration-test/tests/cms/team.test.ts
+++ integration-test/tests/cms/team.test.ts
@@ -17,3 +17,20 @@ test('GET /api/cms/v1/team returns correct data', async ({ request }) => {
     }
   }
 });
+
+test.describe('unauthorized request with invalid headers', () => {
+  
+  const testData = [
+    { description: 'header is empty', headers: { 'X-API-KEY': '' }},
+    { description: 'header is invalid', headers: { 'X-API-KEY': 'invalid_key' }},
+  ];
+
+  testData.forEach(({ description, headers }) => {
+    test(`${description}`, async ({ request }) => {
+      const response = await request.get(`/api/cms/v1/team`, {
+        headers: headers
+      });
+      expect(response.status()).toBe(401);
+    });
+  });
+});
