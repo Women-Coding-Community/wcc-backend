c2ad28d6e3bca6d57712983a3d6a4be2fe5887bc
Nevena Verbic <nevena.verbic@gmail.com>
2025-02-14 08:18:59 +0100

    Test string to enum (#261)
    * test: Add test for StringToEnumConverter.java

--- src/main/java/com/wcc/platform/serializer/StringToEnumConverter.java
+++ src/main/java/com/wcc/platform/serializer/StringToEnumConverter.java
@@ -2,11 +2,12 @@ package com.wcc.platform.serializer;
 
 import com.wcc.platform.domain.platform.ProgramType;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
 
-/** Convert String type (description used as a RequestParam for programs) to Enum ProgramType */
+/** Convert String type (description used as a RequestParam for programs) to Enum ProgramType. */
 public class StringToEnumConverter implements Converter<String, ProgramType> {
   @Override
-  public ProgramType convert(final String description) {
+  public ProgramType convert(@NonNull final String description) {
     return ProgramType.findByValue(description);
   }
 }
