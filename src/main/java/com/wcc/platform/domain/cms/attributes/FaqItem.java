ff1065b0dd42a4a38e91a47d3ad8ce94ef91f31e
Sonali Goel <goelsonali@gmail.com>
2025-02-15 20:01:03 +0000

    review comments

--- src/main/java/com/wcc/platform/domain/cms/attributes/FaqItem.java
+++ src/main/java/com/wcc/platform/domain/cms/attributes/FaqItem.java
@@ -3,9 +3,9 @@ package com.wcc.platform.domain.cms.attributes;
 import jakarta.validation.constraints.NotNull;
 
 /**
- * Represents a FAQ item with a title and description.
+ * Represents a FAQ item with a question and answer.
  *
- * @param title the title of the FAQ item
- * @param description the description of the FAQ item
+ * @param question the question of the FAQ item
+ * @param answer the answer of the FAQ item
  */
-public record FaqItem(@NotNull String title, @NotNull String description) {}
+public record FaqItem(@NotNull String question, @NotNull String answer) {}
