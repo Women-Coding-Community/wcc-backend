474e1084a0e0637397013c513342ac01f32d2bc5
Eleonora Belova <eleonora.belova@Eleonoras-MacBook-Air.local>
2025-03-19 21:30:37 +0100

    feat: added precondition for code of conduct page test case

---
+++ integration-test/utils/datafactory/test-data/codeofconduct.page.data.ts
@@ -0,0 +1,42 @@
+export const codeOfConductPageData = {
+    "id": "page:CODE_OF_CONDUCT",
+    "heroSection": {
+      "title": "WCC Code of Conduct"
+    },
+    "section": {
+      "description": "At Women Coding Community we are committed to a vibrant, supportive community where women can network, share experiences, and foster professional relationships, regardless of age, gender, visible or invisible disability, ethnicity, gender expression, level of experience, education, socio-economic status, nationality, personal appearance, race, religion, sexual orientation or preferred programming language(s)."
+    },
+    "items": [
+      {
+        "title": "Our Standards",
+        "description": "To ensure a positive experience for all members, we expect participants to exhibit the following behaviors:",
+        "items": [
+          "Respect:Treat",
+          "Inclusivity:Actively",
+          "Collaboration:Share",
+          "Constructive Communication: Engage in constructive, positive communication. Provide and gracefully accept constructive criticism.",
+          "Professionalism:Be"
+        ]
+      },
+      {
+        "title": "Unacceptable Behavior",
+        "description": "The following behaviors are considered harassment and are unacceptable within our community:",
+        "items": [
+          "Violence, threats of violence, or violent language directed against another member or group.",
+          "Sexist, racist, homophobic, transphobic, ableist, or otherwise discriminatory jokes and language.",
+          "Personal insults, particularly those related to gender, sexual orientation, race, religion, or disability.",
+          "Inappropriate photography or recording.",
+          "Advocating for, or encouraging, any of the above behavior."
+        ]
+      },
+      {
+        "title": "Enforcement",
+        "description": "Community leaders are responsible for clarifying and enforcing our standards of acceptable behavior and will take appropriate and fair corrective action in response to any behavior that they deem inappropriate, threatening, offensive, or harmful. Instances of abusive, harassing, or otherwise unacceptable behavior can be reported by <a href=\"mailto:london@womencodingcommunity.com\">email</a>. All complaints will be reviewed and investigated promptly and fairly."
+      },
+      {
+        "title": "Consequences",
+        "description": "Actions determined to be contrary to these guidelines may result in consequences ranging from a warning to expulsion from the community, future events depending on the severity of the behavior."
+      }
+    ]
+  }
+  
