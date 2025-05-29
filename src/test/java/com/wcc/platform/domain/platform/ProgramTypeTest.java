da8207c5e167cfcf2604a84afd9e269922e73372
luizagrycuk <luiza.grycuk@ly.st>
2025-05-29 20:59:44 +0100

    Changes following the review: - remove ProgramType test shouldReturnCorrectDescriptionForEnumValues - replace with testToString

--- src/test/java/com/wcc/platform/domain/platform/ProgramTypeTest.java
+++ src/test/java/com/wcc/platform/domain/platform/ProgramTypeTest.java
@@ -2,23 +2,16 @@ package com.wcc.platform.domain.platform;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 /** Test class for {@link ProgramType}. */
 class ProgramTypeTest {
 
-  @Test
-  void shouldReturnCorrectDescriptionForEnumValues() {
-    // When & Then: Check that each enum value has the correct description
-    assertEquals("Book Club", ProgramType.BOOK_CLUB.getDescription());
-    assertEquals("Coding Club Python", ProgramType.CODING_CLUB.getDescription());
-    assertEquals("Career Club", ProgramType.CAREER_CLUB.getDescription());
-    assertEquals("Speaking Club", ProgramType.SPEAKING_CLUB.getDescription());
-    assertEquals("Writing Club", ProgramType.WRITING_CLUB.getDescription());
-    assertEquals("Cloud and DevOps", ProgramType.CLOUD.getDescription());
-    assertEquals("Machine Learning", ProgramType.MACHINE_LEARNING.getDescription());
-    assertEquals("Interview Preparation", ProgramType.INTERVIEW_PREP.getDescription());
-    assertEquals("Others", ProgramType.OTHERS.getDescription());
-    assertEquals("Tech Talk", ProgramType.TECH_TALK.getDescription());
+  @ParameterizedTest
+  @EnumSource(ProgramType.class)
+  void testToString(final ProgramType programType) {
+    final String expected = programType.getDescription();
+    assertEquals(expected, programType.toString());
   }
 }
