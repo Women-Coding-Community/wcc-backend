8a08ac23498280416b52201d6a6e58fdd7639dbb
Adriana Zenck Zimmermann <dricazenck@gmail.com>
2025-06-01 23:08:54 +0200

    Enable Actuator Endpoint (#351)
    * Enable Actuator Endpoints
    * Show OpenApi for Actuator endpoints

--- src/main/java/com/wcc/platform/configuration/OpenApiConfig.java
+++ src/main/java/com/wcc/platform/configuration/OpenApiConfig.java
@@ -4,7 +4,12 @@ import com.wcc.platform.domain.platform.config.PlatformServers;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.tags.Tag;
+import java.util.function.Consumer;
+import java.util.stream.IntStream;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,9 +24,35 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
     in = io.swagger.v3.oas.annotations.enums.SecuritySchemeIn.HEADER)
 public class OpenApiConfig implements WebMvcConfigurer {
 
+  private static final String ACTUATOR = "actuator";
+  private static final String ACTUATOR_SPRING = "Spring Boot Actuator";
+
+  private static Consumer<Tag> renameActuator() {
+    return tag -> {
+      if (ACTUATOR.equalsIgnoreCase(tag.getName())) {
+        tag.setName(ACTUATOR_SPRING);
+      }
+    };
+  }
+
+  private static Consumer<Operation> updateOperationTag() {
+    return operation -> {
+      if (operation.getTags() != null) {
+        IntStream.range(0, operation.getTags().size())
+            .filter(i -> ACTUATOR.equalsIgnoreCase(operation.getTags().get(i)))
+            .forEach(i -> operation.getTags().set(i, ACTUATOR_SPRING));
+      }
+    };
+  }
+
+  /** Group OpenAPI for public APIs. */
   @Bean
   public GroupedOpenApi publicApi() {
-    return GroupedOpenApi.builder().group("public").pathsToMatch("/api/**").build();
+    return GroupedOpenApi.builder()
+        .group("public")
+        .pathsToMatch("/api/**")
+        .addOpenApiCustomizer(renameActuatorTagCustomizer())
+        .build();
   }
 
   /** Customize servers for open API. */
@@ -30,5 +61,21 @@ public class OpenApiConfig implements WebMvcConfigurer {
     return new OpenAPI()
         .addServersItem(new Server().url(PlatformServers.DEV.getUri()).description("Dev"))
         .addServersItem(new Server().url(PlatformServers.LOCAL.getUri()).description("Localhost"));
+  }
+
+  /** Customize Actuator endpoint tag. */
+  private OpenApiCustomizer renameActuatorTagCustomizer() {
+    return openApi -> {
+      if (openApi.getTags() != null) {
+        openApi.getTags().forEach(renameActuator());
+      }
+
+      if (openApi.getPaths() != null) {
+        openApi
+            .getPaths()
+            .values()
+            .forEach(pathItem -> pathItem.readOperations().forEach(updateOperationTag()));
+      }
+    };
   }
 }
