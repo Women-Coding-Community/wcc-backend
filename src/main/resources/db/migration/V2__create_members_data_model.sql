bfe1363b1398aa4b4842079f3f75da00a5ea82b4
Sonali Goel <goelsonali@gmail.com>
2025-06-12 00:22:02 +0100

    updated

--- src/main/resources/db/migration/V2__create_members_data_model.sql
+++ src/main/resources/db/migration/V2__create_members_data_model.sql
@@ -1,5 +1,5 @@
 -- Table for countries
-CREATE TABLE countries
+CREATE TABLE IF NOT EXISTS countries
 (
     id           SERIAL PRIMARY KEY,
     country_code VARCHAR(2) UNIQUE NOT NULL,
@@ -7,14 +7,43 @@ CREATE TABLE countries
 );
 
 -- Table for image types (Desktop, Mobile, Tablet etc)
-CREATE TABLE image_types
+CREATE TABLE IF NOT EXISTS image_types
 (
     id   SERIAL PRIMARY KEY,
     type VARCHAR(50) UNIQUE NOT NULL
 );
 
+
+-- Table for member statuses (Active, Inactive, Banned, etc)
+CREATE TABLE IF NOT EXISTS member_statuses
+(
+    id     SERIAL PRIMARY KEY,
+    status VARCHAR(50) UNIQUE NOT NULL
+);
+
+-- Table for member types (Collaborator, Volunteer, Director, Lead, Evangelist, etc.)
+CREATE TABLE IF NOT EXISTS member_types
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL
+);
+
+-- Table for network types (Twitter, LinkedIn, Medium, etc)
+CREATE TABLE IF NOT EXISTS social_network_types
+(
+    id   SERIAL PRIMARY KEY,
+    type VARCHAR(100) UNIQUE NOT NULL
+);
+
+-- Table for permission types (Admin, Read-Only)
+CREATE TABLE IF NOT EXISTS permission_types
+(
+    id         SERIAL PRIMARY KEY,
+    permission VARCHAR(100) UNIQUE NOT NULL
+);
+
 -- Table for members
-CREATE TABLE members
+CREATE TABLE IF NOT EXISTS members
 (
     id               SERIAL PRIMARY KEY,
     full_name        VARCHAR(255)                             NOT NULL,
@@ -35,50 +64,22 @@ CREATE TABLE members
 );
 
 -- Table for member images
-CREATE TABLE member_images
+CREATE TABLE IF NOT EXISTS member_images
 (
     id            SERIAL PRIMARY KEY,
     member_id     INTEGER REFERENCES members (id) ON DELETE CASCADE,
     image_type_id INTEGER REFERENCES image_types (id),
     image_url     VARCHAR(2048) NOT NULL,
-    image_alt     VARCHAR(250) NOT NULL,
+    image_alt     VARCHAR(250)  NOT NULL,
     created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Table for member networks
-CREATE TABLE member_social_networks
+-- Table for member social networks
+CREATE TABLE IF NOT EXISTS member_social_networks
 (
     id              SERIAL PRIMARY KEY,
     member_id       INTEGER REFERENCES members (id) ON DELETE CASCADE,
-    network_type_id INTEGER REFERENCES network_types (id),
+    network_type_id INTEGER REFERENCES social_network_types (id),
     link            VARCHAR(2048) NOT NULL,
     created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
--- Table for member statuses (Active, Inactive, Banned, etc)
-CREATE TABLE member_statuses
-(
-    id     SERIAL PRIMARY KEY,
-    status VARCHAR(50) UNIQUE NOT NULL
-);
-
--- Table for member types (Collaborator, Volunteer, Director, Lead, Evangelist, etc.)
-CREATE TABLE member_types
-(
-    id   SERIAL PRIMARY KEY,
-    name VARCHAR(100) UNIQUE NOT NULL
-);
-
--- Table for network types (Twitter, LinkedIn, Medium, etc)
-CREATE TABLE social_network_types
-(
-    id      SERIAL PRIMARY KEY,
-    type    VARCHAR(100) UNIQUE NOT NULL
-);
-
--- Table for permission types (Read, Write, Admin, etc)
-CREATE TABLE permission_types
-(
-    id         SERIAL PRIMARY KEY,
-    permission VARCHAR(100) UNIQUE NOT NULL
 );
