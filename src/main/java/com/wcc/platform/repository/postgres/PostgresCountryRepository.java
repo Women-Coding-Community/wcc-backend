77e58d8ac9e6d74f3fb7621c17d9e471c2aacc40
Sonali Goel <112942761+goelsonali@users.noreply.github.com>
2025-07-14 21:17:00 +0100

    Members get api (#367)
    Members repository changes
    
    * update member repository and test
    
    * get and post api working
    
    * fix pmd error
    
    * tests added fixed images
    
    * review comments

---
+++ src/main/java/com/wcc/platform/repository/postgres/PostgresCountryRepository.java
@@ -0,0 +1,56 @@
+package com.wcc.platform.repository.postgres;
+
+import com.wcc.platform.domain.cms.attributes.Country;
+import com.wcc.platform.repository.CrudRepository;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+/** Country data repository */
+@Repository
+@RequiredArgsConstructor
+public class PostgresCountryRepository implements CrudRepository<Country, Long> {
+
+  private final JdbcTemplate jdbc;
+
+  @Override
+  public Country create(final Country entity) {
+    final String sql = "INSERT INTO countries (country_code, country_name) VALUES (?, ?)";
+    jdbc.update(sql, entity.countryCode(), entity.countryName());
+    return entity;
+  }
+
+  @Override
+  public Country update(final Long id, final Country entity) {
+    final String sql = "UPDATE countries SET country_name = ? AND country_code = ? WHERE id = ?";
+    jdbc.update(sql, entity.countryName(), entity.countryCode(), id);
+    return entity;
+  }
+
+  @Override
+  public Optional<Country> findById(final Long id) {
+    final String sql = "SELECT * FROM countries WHERE id = ?";
+    return jdbc.query(
+        sql,
+        rs -> {
+          if (rs.next()) {
+            return Optional.of(
+                new Country(rs.getString("country_code"), rs.getString("country_name")));
+          }
+          return Optional.empty();
+        },
+        id);
+  }
+
+  @Override
+  public void deleteById(final Long id) {
+    jdbc.update("DELETE FROM countries WHERE id = ?", id);
+  }
+
+  /** Retrieves the country ID associated with the specified country code. */
+  public Long findCountryIdByCode(final String countryCode) {
+    final String sql = "SELECT id FROM countries WHERE country_code = ?";
+    return jdbc.queryForObject(sql, Long.class, countryCode);
+  }
+}
