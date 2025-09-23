bc8fd9da9695fd61861d962dfec2637106f9d08a
Sonali Goel <goelsonali@gmail.com>
2025-09-23 21:44:04 +0100

    more tables

--- src/main/java/com/wcc/platform/domain/platform/mentorship/MentorshipType.java
+++ src/main/java/com/wcc/platform/domain/platform/mentorship/MentorshipType.java
@@ -1,8 +1,15 @@
 package com.wcc.platform.domain.platform.mentorship;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /* Type of mentorship cycle */
+@Getter
+@AllArgsConstructor
 public enum MentorshipType {
-  AD_HOC,
-  LONG_TERM,
-  STUDY_GROUP
+  AD_HOC(1),
+  LONG_TERM(2),
+  STUDY_GROUP(3);
+
+  private final int mentorshipTypeId;
 }
