45242257de15b82cd43718656797b908514c5605
Adriana Zenck Zimmermann <dricazenck@gmail.com>
2025-09-27 09:10:56 +0200

    Disable database and use file all fallback (#385)
    In order to run the application in fly.io it was necessary to use in memory database, so we don't depend on Postgres.
    
    This should be a temporary solution to unblock frontend teams to use the latest version of the apis.

--- src/main/resources/db/migration/V3__20250613__update_members_data_model.sql
+++ src/main/resources/db/migration/V3__20250613__update_members_data_model.sql
@@ -1,4 +1,4 @@
 -- Update the MEMBERS table to remove permission_id and set email as NOT NULL
-ALTER TABLE MEMBERS
-    DROP COLUMN permission_id,
-    ALTER COLUMN email SET NOT NULL;
+-- Make it compatible with both PostgreSQL and H2 (used on Fly.io)
+ALTER TABLE members DROP COLUMN IF EXISTS permission_id;
+ALTER TABLE members ALTER COLUMN email SET NOT NULL;
