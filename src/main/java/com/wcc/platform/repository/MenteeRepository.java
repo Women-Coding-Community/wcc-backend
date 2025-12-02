ae32447aeb17f38f434374752c8ffe991ff0a778
ValeriaM <val.mix465@gmail.com>
2025-12-02 14:39:05 +0000

    Remove unused imports.

--- src/main/java/com/wcc/platform/repository/MenteeRepository.java
+++ src/main/java/com/wcc/platform/repository/MenteeRepository.java
@@ -1,9 +1,7 @@
 package com.wcc.platform.repository;
 
-import com.wcc.platform.domain.platform.member.Member;
 import com.wcc.platform.domain.platform.mentorship.Mentee;
 import java.util.List;
-import java.util.Optional;
 
 /**
  * Repository interface for managing mentees entities. Provides methods to perform CRUD operations
