1bca45b222ef9f9d4e2995d9c46a751ba2bce42a
ValeriaM <val.mix465@gmail.com>
2026-01-12 21:27:32 +0000

    Move the join table from the V16 migration file to V15. Remove the V16 migration file.

--- src/main/resources/db/migration/V15__20251117__create_mentees_data_model.sql
+++ src/main/resources/db/migration/V15__20251117__create_mentees_data_model.sql
@@ -45,4 +45,12 @@ CREATE TABLE mentee_technical_areas
     PRIMARY KEY (mentee_id, technical_area_id)
 );
 
+-- TABLE Mentee mentorship focus areas (join table)
+CREATE TABLE mentee_mentorship_focus_areas
+(
+    mentee_id     INTEGER REFERENCES mentees (mentee_id) ON DELETE CASCADE,
+    focus_area_id INTEGER REFERENCES mentorship_focus_areas (id) ON DELETE CASCADE,
+    PRIMARY KEY (mentee_id, focus_area_id)
+);
+
 -- 🔹 Seed data inserts
--- src/main/resources/db/migration/V16__20251128__create_mentee_mentorship_focus.sql
+++
@@ -1,8 +0,0 @@
-
--- TABLE Mentee mentorship focus areas (join table)
-CREATE TABLE mentee_mentorship_focus_areas
-(
-    mentee_id     INTEGER REFERENCES mentees (mentee_id) ON DELETE CASCADE,
-    focus_area_id INTEGER REFERENCES mentorship_focus_areas (id) ON DELETE CASCADE,
-    PRIMARY KEY (mentee_id, focus_area_id)
-);
