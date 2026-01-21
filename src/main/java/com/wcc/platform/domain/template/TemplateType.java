23ed3d6c3073263da34978745af3fbb73bff84ac
ValeriaM <val.mix465@gmail.com>
2026-01-21 00:25:39 +0000

    Add two yml files with the join new cycle and welcome mentorship email templates.

--- src/main/java/com/wcc/platform/domain/template/TemplateType.java
+++ src/main/java/com/wcc/platform/domain/template/TemplateType.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 @Getter
 public enum TemplateType {
   FEEDBACK_MENTOR_ADHOC("feedback_mentor_adhoc.yml"),
-  FEEDBACK_MENTOR_LONG("feedback_mentor_long_term.yml");
+  FEEDBACK_MENTOR_LONG("feedback_mentor_long_term.yml"),
+  NEW_CYCLE_MENTOR_LONG("joining_new_mentorship_cycle_mentor_long_term.yml"),
+  WELCOME_MENTORSHIP_MENTEE_MENTOR_LONG("welcome_mentorship_mentee_mentor_long_term.yml");
 
   private final String templateFile;
 
