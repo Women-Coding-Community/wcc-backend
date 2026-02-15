2705a16587667fdc773d7f618913ecbfd73d54fc
Laura Cabantous-Taylor <laura.cabantous@gmail.com>
2026-02-15 19:50:18 +0000

    Suppress Pmd warning for long variable on Template Type

--- src/main/java/com/wcc/platform/domain/template/TemplateType.java
+++ src/main/java/com/wcc/platform/domain/template/TemplateType.java
@@ -3,6 +3,7 @@ package com.wcc.platform.domain.template;
 import lombok.Getter;
 
 @Getter
+@SuppressWarnings("PMD.LongVariable")
 public enum TemplateType {
   FEEDBACK_MENTOR_ADHOC("feedback_mentor_adhoc.yml"),
   FEEDBACK_MENTOR_LONG("feedback_mentor_long_term.yml"),
