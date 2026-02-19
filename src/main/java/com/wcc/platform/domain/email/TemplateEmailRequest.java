1f2a7af4cf215927d51615edfc50dda27deb1cd4
ValeriaM <val.mix465@gmail.com>
2026-02-19 22:02:44 +0000

    Change the validation from NotBlank to NotNull on the templateType and templateParameters fields.

--- src/main/java/com/wcc/platform/domain/email/TemplateEmailRequest.java
+++ src/main/java/com/wcc/platform/domain/email/TemplateEmailRequest.java
@@ -4,7 +4,7 @@ import com.wcc.platform.domain.template.TemplateType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import java.util.Map;
 import lombok.AllArgsConstructor;
@@ -19,6 +19,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Schema(description = "Request object for sending an email using template")
 public class TemplateEmailRequest {
+
     @NotBlank(message = "Recipient email is required")
     @Email(message = "Recipient email must be valid")
     @Schema(description = "Recipient email address", example = "recipient@example.com")
@@ -30,19 +31,19 @@ public class TemplateEmailRequest {
     @Schema(description = "BCC recipients", example = "[\"bcc@example.com\"]")
     private List<String> bcc;
 
-    @NotBlank(message = "Template type is required")
+    @NotNull(message = "Template type is required")
     private TemplateType templateType;
 
-    @NotBlank(message = "Template parameters are required")
+    @NotNull(message = "Template parameters are required")
     @Schema(description = "Key-value pairs used to replace placeholders inside the email template",
         example = """
-    {
-      "mentorName": "Mike",
-      "program": "Ad-hoc mentorship",
-      "menteeName": "Alice",
-      "deadline": "27/09/25"
-    }
-    """)
+            {
+              "mentorName": "Mike",
+              "program": "Ad-hoc mentorship",
+              "menteeName": "Alice",
+              "deadline": "27/09/25"
+            }
+            """)
     private Map<String, String> templateParameters;
 
     @Schema(
