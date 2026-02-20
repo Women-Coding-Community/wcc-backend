ca54b9469d9f681b6fd1c56980521bbb60fba4ef
Laura Cabantous-Taylor <laura.cabantous@gmail.com>
2026-02-20 17:43:40 +0000

    Update MentorDeserializationTest class documentation

--- src/test/java/com/wcc/platform/deserializers/MentorDeserializationTest.java
+++ src/test/java/com/wcc/platform/deserializers/MentorDeserializationTest.java
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-/** Unit tests for Mentor deserialization with custom MonthDeserializer. */
+/** Unit tests for Mentor deserialization. */
 class MentorDeserializationTest {
 
   private ObjectMapper objectMapper;
