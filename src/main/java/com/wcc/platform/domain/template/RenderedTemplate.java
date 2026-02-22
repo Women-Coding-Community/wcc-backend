e0b4db68d747fc168595029a9ad32e92a6fece39
ValeriaM <val.mix465@gmail.com>
2026-02-22 09:38:09 +0000

    Remove the ignore property from RenderedTemplate.

--- src/main/java/com/wcc/platform/domain/template/RenderedTemplate.java
+++ src/main/java/com/wcc/platform/domain/template/RenderedTemplate.java
@@ -1,5 +1,4 @@
 package com.wcc.platform.domain.template;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 /**
  * RenderedTemplate record representing a rendered email template.
@@ -7,7 +6,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
  * @param subject
  * @param body
  */
-@JsonIgnoreProperties(ignoreUnknown = true)
 public record RenderedTemplate(String subject, String body) {
   public static RenderedTemplate from(final Template template) {
     return new RenderedTemplate(template.subject(), template.body());
