5c85c021086d4e6b870e939cb7b5929c1300f302
nora-weisser <eleo.belova@yandex.ru>
2026-04-02 21:38:45 +0200

    feat: addressed the comment

--- integration-test/utils/datafactory/schemas/user.account.schema.ts
+++ integration-test/utils/datafactory/schemas/user.account.schema.ts
@@ -1,20 +1,7 @@
 import { z } from 'zod';
 import { roleTypeSchema } from './auth.schema';
 
-const permissionSchema = z.enum([
-  'USER_READ',
-  'USER_WRITE',
-  'USER_DELETE',
-  'MENTOR_APPL_READ',
-  'MENTOR_APPL_WRITE',
-  'MENTOR_PROFILE_UPDATE',
-  'MENTEE_APPL_SUBMIT',
-  'MENTEE_APPL_READ',
-  'MENTOR_APPROVE',
-  'MENTEE_APPROVE',
-  'CYCLE_EMAIL_SEND',
-  'MATCH_MANAGE',
-]);
+const permissionSchema = z.string().min(1);
 
 export const userAccountSchema = z.object({
   id: z.number(),
